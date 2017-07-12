364bfa7593ab95b63cc515bc91d707b40889ce4f
tbskybj <2311943591@qq.com>
2017-07-12 12:45:05 +0800

    分支提交

--- src/main/java/com/didispace/Application.java
+++ src/main/java/com/didispace/Application.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class Application {
 
 	public static void main(String[] args) {
+		System.out.println("这是新分支");
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
 	}
 
